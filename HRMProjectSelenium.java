d90ebf3f85bc1c4bca519b84ad6c8957e695930c
Siddharth Udgata <siddharthu20@gmail.com>
2018-04-05 16:21:04 +0530

    Add files via upload

---
+++ HRMProjectSelenium.java
@@ -0,0 +1,64 @@
+package HRMProject_Selenium;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class HRMProjectSelenium {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	System.setProperty("webdriver.ie.driver", "H:/Desktop/New folder/eclipse and selenium/IEDriverServer.exe");
+		
+		WebDriver driver = new InternetExplorerDriver();
+		
+         driver.manage().timeouts().implicitlyWait(12, TimeUnit.SECONDS);
+		
+		driver.get("http://opensource.demo.orangehrmlive.com/");
+		
+       WebDriverWait wait2 = new WebDriverWait(driver, 10);
+		
+		wait2.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//*[@id='divLogo']/img")));
+		
+		driver.findElement(By.xpath(".//*[@id='txtUsername']")).sendKeys("Admin");
+		
+		driver.findElement(By.xpath(".//*[@id='txtPassword']")).sendKeys("admin");
+		
+		driver.findElement(By.xpath(".//*[@id='btnLogin']")).click();
+		
+		WebDriverWait wait1 = new WebDriverWait(driver,10);
+		
+		wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//*[@id='menu_recruitment_viewRecruitmentModule']/b")));
+		
+		driver.findElement(By.xpath(".//*[@id='menu_recruitment_viewRecruitmentModule']/b")).click();
+		
+		driver.findElement(By.xpath(".//*[@id='menu_recruitment_viewJobVacancy']")).click();
+
+		driver.findElement(By.xpath(".//*[@id='btnAdd']")).click();
+		
+	   Select dropdown = new Select(driver.findElement(By.xpath(".//*[@id='addJobVacancy_jobTitle']")));
+	   
+	   dropdown.selectByIndex(2);
+	   
+	   driver.findElement(By.xpath(".//*[@id='addJobVacancy_name']")).sendKeys("Technology Analyst");
+	   
+	   driver.findElement(By.xpath(".//*[@id='addJobVacancy_hiringManager']")).sendKeys("Allen Smith");
+	   
+	   driver.findElement(By.xpath(".//*[@id='addJobVacancy_noOfPositions']")).sendKeys("2");
+	   
+	   driver.findElement(By.xpath(".//*[@id='btnSave']")).click();
+	   
+	   
+	   driver.findElement(By.xpath(".//*[@id='btnBack']")).click();
+	   
+	   driver.findElement(By.xpath(".//*[@id='resultTable']/tbody/tr[2]/td[2]/a")).click();
+	 
+	}
+
+}
